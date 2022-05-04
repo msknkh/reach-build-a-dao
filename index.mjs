1a23e2fb458bb92b30db9aab072dfc2340946c61
msknkh <muskankhandelwal369@gmail.com>
2022-05-04 18:33:55 +0530

    Incomplete version with tests

--- index.mjs
+++ index.mjs
@@ -1,31 +1,84 @@
-import { loadStdlib } from '@reach-sh/stdlib';
-import * as backend from './build/index.main.mjs';
+// import { loadStdlib } from '@reach-sh/stdlib';
+// import * as backend from './build/index.main.mjs';
 
-const stdlib = loadStdlib();
-const startingBalance = stdlib.parseCurrency(100);
-const acc = await stdlib.newTestAccount(startingBalance);
+// const stdlib = loadStdlib();
+// const startingBalance = stdlib.parseCurrency(100);
+// const accD = await stdlib.newTestAccount(startingBalance);
 
-const ctc = acc.contract(backend);
+// //const deadline = stdlib.connector === 'CFX' ? 500 : 250;
 
-const proposers = [];
-const startProposers = async () => {
+// const ctcD = accD.contract(backend);
 
-    const runProposers = async (who) => {
-        const acc = await stdlib.newTestAccount(startingBalance);
-        acc.setDebugLabel(who);
-        proposers.push([who, acc]);
+// try {
 
-        console.log(`${who} decides to propose`);
-    }
+//     await ctcD.p.Admin({
+//         //deadline,
+//         price: stdlib.parseCurrency(25),
+//         ready: () => {
+//             console.log('The contract is ready');
+//             throw 42;
+//         }
+//     });
 
-    await runProposers('Alice');
-    await runProposers('Bob');
+// } catch (e) {
+//     if (e != 42) {
+//         throw e;
+//     }
+// }
 
-}
+// const users = await stdlib.newTestAccount(5, sbal);
 
-await ctc.participants.Creator({
-    creationReady: () => {
-        startProposers();
-    },
+// const willError = async (f) => {
+//     let e;
+//     try {
+//         await f();
+//         e = false;
+//     } catch (te) {
+//         e = te;
+//     }
 
-});
+//     if (e === false) {
+//         throw Error('Expected to error, but didnt');
+//     }
+
+//     console.log(`That last call errored and that's okay`);
+
+// };
+
+// const ctcWho = (whoi) => 
+//     users[whoi].contract(backend, ctcD.getInfo());
+
+// const rsvp = async (whoi) => {
+//     const who = users[whoi];
+//     const ctc = ctcWho(whoi);
+//     console.log('RSVP of', stdlib.formatAddress(who));
+//     await ctc.apis.Proposer.propose();
+// }
+
+// const do_checkin = async (ctc, whoi) => {
+//     const who = users[whoi];
+//     console.log('Check in of', stdlib.formatAddress(who));
+//     await ctc.apis.Voter.vote(who);
+// };
+
+// const checkin = (whoi) => do_checkin(ctcD, whoi);
+// const bad_checkin = (whoi) => do_checkin(ctcWho(whoi), whoi);
+// const timesup = async () => {
+//     console.log('I think time is up');
+//     await ctcD.apis.Voter.timesup();
+// }
+
+// await rsvp(0);
+// await rsvp(1);
+// await willError(() => rsvp(1));
+// await checkin(4);
+
+// console.log('we are gonna wait for the deadline');
+// await stdlib.wait(deadline);
+
+// await timesup();
+
+// for (const who of [accD, ...users]) {
+//     console.warn(stdlib.formatAddress(who), 'has', 
+//         stdlib.formatCurrency(await stdlib.balanceof(who)));
+// }
--- index.rsh
+++ index.rsh
@@ -1,18 +1,78 @@
 'reach 0.1';
 
 export const main = Reach.App(() => {
-    const Creator = Participant('Creator', {
-        creationReady: Fun([], Null),
+    const Deployer = Participant('Deployer', {
+        signedMoney: UInt,
+        deadline: UInt,
+        deployReady: Fun([], Null),
     });
 
+    //Proposer Address, description 
+    //UInt
     const Proposer = API('Proposer', {
-
+        propose: Fun([Address], Bool),
     });
 
+    //proposalId, vote, reason, Address
+    //UInt, UInt, 
     const Voter = API('Voter', {
-
+        //0 -> yes
+        //1 -> no 
+        //2 -> abstain
+        vote: Fun([Address], Bool),
     });
 
     init()
+
+    Deployer.only(() => {
+        const signedMoney = declassify(interact.signedMoney);
+        const deadline = declassify(interact.deadline);
+    })
+    Deployer.publish(signedMoney, deadline);
+    commit();
+    Deployer.publish();
+    Deployer.interact.deployReady();
+
+    const deadlineBlock = relativeTime(deadline);
+    //const proposals = new Set();
+    const Votes = new Set(); 
+
+
+    const [ keepGoing, howMany ] = 
+        parallelReduce([true, 0])
+        .define(() => {
+            const checkVoted = (actor, who) => {
+                check( actor == Deployer, "you are the boss");
+                check( ! Votes.member(who), "not yet" );
+                return () => {
+                  Votes.insert(who);
+                  return [ keepGoing, howMany + 1 ];
+                };
+            };
+        })
+        .invariant(
+            balance() == howMany * signedMoney
+            && Votes.Map.size() == howMany
+        )
+        .while( keepGoing )
+        .api(Proposer.propose,
+            (who) => {},
+            (_) => 0,
+            (who, k) => {
+                k(true);
+                return [true, howMany];
+            }
+        ) 
+        .api(Voter.vote,
+            (who) => {const _ = checkVoted(this, who); },
+            (_) => signedMoney,
+            (who, k) => {
+                k(true);
+                return checkVoted(this, who)();
+        }) 
+
+
+    commit();
+    exit();
 
 });
